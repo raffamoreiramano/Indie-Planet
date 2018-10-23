334de680d209d778a82e90321cae6a5f4aac787f
Azdriam <39443416+Azdriam@users.noreply.github.com>
2018-10-23 15:30:18 -0300

    Adrian commit 2
    * Add files via upload
    
    * Add files via upload
    
    * Add files via upload
    
    porra mano tomara q fique bom
    
    * Add files via upload

--- animacoes/top_10/index.php
+++ animacoes/top_10/index.php
@@ -7,99 +7,100 @@
     include $corredor.'includes/head.php';
 ?>
 
-    <body class="<?php echo $pastaAtual; ?>">
-        <header>
-            <?php
+<body class="<?php echo $pastaAtual; ?>">
+    <header>
+        <?php
                 include $corredor.'includes/nav-top.php';
             ?>
-            <?php
+        <?php
                 include $corredor.'includes/nav-mid.php';
             ?>
-        </header>
-        <?php
+    </header>
+    <?php
             include $corredor.'includes/trail.php';
         ?>
-        <div class="container">
-            <div class="row">
-                <div class="col-12 col-md-12 col-lg-6"> 
-            <p> Aqui esta o Top 10 das melhores animações indie e de quebra música indie</p>
-                </div>
+    <div class="container">
+        <div class="row">
+            <div class="col-12 col-md-12 col-lg-12">
+                <h1> Aqui esta o Top 10 das melhores animações indie e de quebra música indie</h1>
             </div>
-            
-    <div class="row">
-        <div class="col-12 col-md-6 col-lg-6">
-            <p>1.Gorillaz-Saturnz barnz</p>
+        </div>
+
+
+        <div class="row">
+            <div class="col-12 col-md-6 col-lg-6 p-0 mb-2 b-2" style="background-color:  #8BCCF2; border-right-color: white; border-right-style: solid; ">
+                <p class="mb-0" style="color: white; text-transform: uppercase; font-weight: bolder; text-align: center;">1.Gorillaz-Saturnz barnz</p>
                 <div class="embed-responsive embed-responsive-16by9">
                     <iframe class="embed-responsive-item" src="https://www.youtube.com/embed/5qJp6xlKEug" allowfullscreen></iframe>
                 </div>
-        </div>
-    <div class="col-12 col-md-6 col-lg-6">
-            <p>2.SIAMES-the wolf</p>
+            </div>
+            <div class="col-12 col-md-6 col-lg-6 p-0 mb-2 b-2" style="background-color:  #8BCCF2; border-right-color: white; border-right-style: solid; ">
+                <p class="mb-0" style="color: white; text-transform: uppercase; font-weight: bolder; text-align: center;">2.SIAMES-the wolf</p>
                 <div class="embed-responsive embed-responsive-16by9">
                     <iframe class="embed-responsi   ve-item" src="https://www.youtube.com/embed/lX44CAz-JhU" allowfullscreen></iframe>
                 </div>
+            </div>
         </div>
-</div>        
-<div class="row">
-        <div class="col-12 col-md-6 col-lg-6">
-            <p>3.Gorillaz- feel good inc.</p>
+        <div class="row">
+            <div class="col-12 col-md-6 col-lg-6 p-0 mb-2 b-2" style="background-color:  #8BCCF2; border-right-color: white; border-right-style: solid; ">
+                <p class="mb-0" style="color: white; text-transform: uppercase; font-weight: bolder; text-align: center;">3.Gorillaz- feel good inc.</p>
                 <div class="embed-responsive embed-responsive-16by9">
                     <iframe class="embed-responsive-item" src="https://www.youtube.com/embed/HyHNuVaZJ-k" allowfullscreen></iframe>
                 </div>
-        </div>
-    <div class="col-12 col-md-6 col-lg-6">
-            <p>4.Concorde- sons</p>
+            </div>
+            <div class="col-12 col-md-6 col-lg-6 p-0 mb-2 b-2" style="background-color:  #8BCCF2; border-right-color: white; border-right-style: solid; ">
+                <p class="mb-0" style="color: white; text-transform: uppercase; font-weight: bolder; text-align: center;">4.Concorde- sons</p>
                 <div class="embed-responsive embed-responsive-16by9">
                     <iframe class="embed-responsive-item" src="https://www.youtube.com/embed/ZeBrnuQxEsQ" allowfullscreen></iframe>
                 </div>
+            </div>
         </div>
-</div>        
-<div class="row">
-        <div class="col-12 col-md-6 col-lg-6">
-            <p>5.Gorillaz-clint eastwood</p>
+        <div class="row">
+            <div class="col-12 col-md-6 col-lg-6 p-0 mb-2 b-2" style="background-color:  #8BCCF2; border-right-color: white; border-right-style: solid; ">
+                <p class="mb-0" style="color: white; text-transform: uppercase; font-weight: bolder; text-align: center;">5.Gorillaz-clint eastwood</p>
                 <div class="embed-responsive embed-responsive-16by9">
                     <iframe class="embed-responsive-item" src="https://www.youtube.com/embed/1V_xRb0x9aw" allowfullscreen></iframe>
                 </div>
-        </div>
-    <div class="col-12 col-md-6 col-lg-6">
-            <p>6.Stuck in the sound- Lets Go</p>
+            </div>
+            <div class="col-12 col-md-6 col-lg-6 p-0 mb-2 b-2" style="background-color:  #8BCCF2; border-right-color: white; border-right-style: solid; ">
+                <p class="mb-0" style="color: white; text-transform: uppercase; font-weight: bolder; text-align: center;">6.Stuck in the sound- Lets Go</p>
                 <div class="embed-responsive embed-responsive-16by9">
                     <iframe class="embed-responsive-item" src="https://www.youtube.com/embed/52Gg9CqhbP8" allowfullscreen></iframe>
                 </div>
+            </div>
         </div>
-</div>        
-<div class="row">
-        <div class="col-12 col-md-6 col-lg-6">
-            <p>7.Gorillaz-19-2000</p>
+        <div class="row">
+            <div class="col-12 col-md-6 col-lg-6 p-0 mb-2 b-2" style="background-color:  #8BCCF2; border-right-color: white; border-right-style: solid; ">
+                <p class="mb-0" style="color: white; text-transform: uppercase; font-weight: bolder; text-align: center;">7.Gorillaz-19-2000</p>
                 <div class="embed-responsive embed-responsive-16by9">
                     <iframe class="embed-responsive-item" src="https://www.youtube.com/embed/WXR-bCF5dbM" allowfullscreen></iframe>
                 </div>
-        </div>
-    <div class="col-12 col-md-6 col-lg-6">
-            <p>8.artic Monkeys- Do I wanna Know</p>
+            </div>
+            <div class="col-12 col-md-6 col-lg-6 p-0 mb-2 b-2" style="background-color:  #8BCCF2; border-right-color: white; border-right-style: solid; ">
+                <p class="mb-0" style="color: white; text-transform: uppercase; font-weight: bolder; text-align: center;">8.artic Monkeys- Do I wanna Know</p>
                 <div class="embed-responsive embed-responsive-16by9">
                     <iframe class="embed-responsive-item" src="https://www.youtube.com/embed/bpOSxM0rNPM" allowfullscreen></iframe>
                 </div>
+            </div>
         </div>
-</div>        
-<div class="row mb-3">
-        <div class="col-12 col-md-6 col-lg-6">
-            <p>9.foster the people- Pseudologia Fantastica</p>
+        <div class="row">
+            <div class="col-12 col-md-6 col-lg-6 p-0 mb-2 b-2" style="background-color:  #8BCCF2; border-right-color: white; border-right-style: solid; ">
+                <p class="mb-0" style="color: white; text-transform: uppercase; font-weight: bolder; text-align: center;">9.foster the people- Pseudologia Fantastica</p>
                 <div class="embed-responsive embed-responsive-16by9">
                     <iframe class="embed-responsive-item" src="https://www.youtube.com/embed/5-mTcFGiVzw" allowfullscreen></iframe>
                 </div>
-        </div>
-    <div class="col-12 col-md-6 col-lg-6">
-            <p>10.CRUISR - All Over</p>
+            </div>
+            <div class="col-12 col-md-6 col-lg-6 p-0 mb-2 b-2" style="background-color:  #8BCCF2; border-right-color: white; border-right-style: solid; ">
+                <p class="mb-0" style="color: white; text-transform: uppercase; font-weight: bolder; text-align: center;">10.CRUISR - All Over</p>
                 <div class="embed-responsive embed-responsive-16by9">
                     <iframe class="embed-resp0onsive-item" src="https://www.youtube.com/embed/pKemSP-Ei58" allowfullscreen></iframe>
                 </div>
+            </div>
         </div>
-</div>    
-        </div>
-        <?php
+    </div>
+    <?php
             include $corredor.'includes/footer.php';
-        ?>    
-    </body>
+        ?>
+</body>
 
 </html>
