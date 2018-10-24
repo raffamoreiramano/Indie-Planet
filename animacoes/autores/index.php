f944492bfc16bd233290f10d634e0740ca5e8ac9
Azdriam <39443416+Azdriam@users.noreply.github.com>
2018-10-24 12:48:49 -0300

    correção das linkagens erradas

--- animacoes/autores/index.php
+++ animacoes/autores/index.php
@@ -22,37 +22,37 @@
 
 <div class="container">
     
-    <div class="row">
-        <div class="col-12 col-md-5 col-lg-5">
-            <h1>Artista do mes: jamie hewlett</h1>
-            <p>Integer porttitor suscipit tincidunt. In luctus accumsan feugiat. Donec in nibh nec dolor viverra tincidunt. porttitor suscipit tincidunt. Integer porttitor suscipit tincidunt. In luctus accumsan feugiat. Donec in nibh nec dolor viverra tincidunt. porttitor suscipit tinciduntInteger porttitor suscipit tincidunt. In luctus accumsan feugiat. Donec in nibh nec dolor viverra tincidunt. porttitor suscipit tincidunt</p>
-        </div>
-        <div class="col-12 col-md-7 col-lg-7">
-            <img class="img-fluid width:100% rounded mx-auto d-block" alt="jamie hewlett" src="img/autor1.jpg">
-        </div>
-    </div>
-
-    <div class="row">
-        <div class="col-12 col-md-12 col-lg-12">
-            <p>Integer porttitor suscipit tincidunt. In luctus accumsan feugiat. Donec in nibh nec dolor viverra tincidunt. porttitor suscipit tincidunt. Integer porttitor suscipit tincidunt. In luctus accumsan feugiat. Donec in nibh nec dolor viverra tincidunt. porttitor suscipit tinciduntInteger porttitor suscipit tincidunt. In luctus accumsan feugiat. Donec in nibh nec dolor viverra tincidunt. porttitor suscipit tincidunt Integer porttitor suscipit tincidunt. In luctus accumsan feugiat. Donec in nibh nec dolor viverra tincidunt. porttitor suscipit tinciduntInteger porttitor suscipit tincidunt. In luctus accumsan feugiat. Donec in nibh nec dolor viverra tincidunt. porttitor suscipit tincidunt Integer porttitor suscipit tincidunt. In luctus accumsan feugiat. Donec in nibh nec dolor viverra tincidunt. porttitor suscipit tinciduntInteger porttitor suscipit tincidunt. In luctus accumsan feugiat. Donec in nibh nec dolor viverra tincidunt. porttitor suscipit tincidunt</p>
+       <div class="row">
+            <div class="col-12 col-md-5 col-lg-5">
+                <h1>Artista do mes: jamie hewlett</h1>
+                <p>Integer porttitor suscipit tincidunt. In luctus accumsan feugiat. Donec in nibh nec dolor viverra tincidunt. porttitor suscipit tincidunt. Integer porttitor suscipit tincidunt. In luctus accumsan feugiat. Donec in nibh nec dolor viverra tincidunt. porttitor suscipit tinciduntInteger porttitor suscipit tincidunt. In luctus accumsan feugiat. Donec in nibh nec dolor viverra tincidunt. porttitor suscipit tincidunt</p>
+            </div>
+            <div class="col-12 col-md-7 col-lg-7">
+                <img class="img-fluid width:100% rounded mx-auto d-block" alt="shaunteste" src="../../img/jamiehewlett.jpg">
+            </div>
         </div>
 
-    </div>
+        <div class="row">
+            <div class="col-12 col-md-12 col-lg-12">
+                <p>Integer porttitor suscipit tincidunt. In luctus accumsan feugiat. Donec in nibh nec dolor viverra tincidunt. porttitor suscipit tincidunt. Integer porttitor suscipit tincidunt. In luctus accumsan feugiat. Donec in nibh nec dolor viverra tincidunt. porttitor suscipit tinciduntInteger porttitor suscipit tincidunt. In luctus accumsan feugiat. Donec in nibh nec dolor viverra tincidunt. porttitor suscipit tincidunt Integer porttitor suscipit tincidunt. In luctus accumsan feugiat. Donec in nibh nec dolor viverra tincidunt. porttitor suscipit tinciduntInteger porttitor suscipit tincidunt. In luctus accumsan feugiat. Donec in nibh nec dolor viverra tincidunt. porttitor suscipit tincidunt Integer porttitor suscipit tincidunt. In luctus accumsan feugiat. Donec in nibh nec dolor viverra tincidunt. porttitor suscipit tinciduntInteger porttitor suscipit tincidunt. In luctus accumsan feugiat. Donec in nibh nec dolor viverra tincidunt. porttitor suscipit tincidunt</p>
+            </div>
 
-    <div class="row">
-        <div class="col-12 col-md-12 col-lg-12">
-            <h1>desenhos do jamie hewlett</h1>
         </div>
-        <div class="col-12 col-md-4 col-lg-4">
-            <img class="img-fluid width:100% rounded mx-auto d-block" alt="gorillazold" src="img/gorillaz%20new.jpg">
+
+     <div class="row">
+         <div class="col-12 col-md-12 col-lg-12">
+         <h1>desenhos do jamie hewlett</h1>
+         </div>
+            <div class="col-12 col-md-4 col-lg-4 mb-3">
+                <img class="img-fluid width:100% rounded mx-auto d-block" alt="shaunteste" src="../../img/gorillaznew.jpg">
+            </div> 
+         <div class="col-12 col-md-4 col-lg-4 mb-3">
+                <img class="img-fluid width:100% rounded mx-auto d-block" alt="shaunteste" src="../../img/gorillazold.jpg">
+            </div> 
+         <div class="col-12 col-md-4 col-lg-4 mb-3">
+                <img class="img-fluid width:100% rounded mx-auto d-block" alt="shaunteste" src="../../img/tankgirl.jpg">
+            </div>
         </div>
-        <div class="col-12 col-md-4 col-lg-4">
-            <img class="img-fluid width:100% rounded mx-auto d-block" alt="gorillaznew" src="img/gorillaz%20old.jpg">
-        </div>
-        <div class="col-12 col-md-4 col-lg-4">
-            <img class="img-fluid width:100% rounded mx-auto d-block" alt="tankgirl" src="img/tank%20girl.jpg">
-        </div>
-    </div>
     
     </div>
 
