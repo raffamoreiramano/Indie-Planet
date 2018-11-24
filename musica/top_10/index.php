8ef9c29a08586e0427c77c4452ed46beb5030c0c
Rafa Leme <unkindledone@hotmail.com>
2018-11-24 17:30:25 -0200

    Correção do Top 10 Músicas

--- musica/top_10/index.php
+++ musica/top_10/index.php
@@ -56,38 +56,6 @@
                     <div class="row">
                         <iframe class='border-0 w-100 spotify' src="https://open.spotify.com/embed/user/22f6qy24qivc57h6keamwxhli/playlist/<?php echo $spotifyPlaylist; ?>"></iframe>
                     </div>
-                    <div class='aside-content row bg-dark p-3 mt-4'>
-                        <h4 class='h4 col my-auto font-weight-bold px-0'>Sugestões</h4>
-                    </div>
-                    <div class='row'>
-                        <div class="col feed">
-                            <?php
-                                $news['titles']=array(
-                                    "Reconhecimento da cultura indie",
-                                    "Festivais indie de responsa",
-                                    "Títulos indie que não perdem pra qualquer AAA",
-                                    "O Rumo da Indústria",
-                                    "Artista brasileiro ganha prêmio de melhor título indie"
-                                );
-                                $news['areas']=array("Música","Cinema","Séries","Animações","Jogos");
-                                $news['dates']=array("seis","duas","três","quatro","cinco");
-                            
-                                for ($i=0;$i<5;$i++) {
-                                    echo "<article class='container-fluid pr-0 pl-lg-0 my-3'>\n<div class='row'>\n<a href='";
-                                    echo $corredor.strtolower(pato($news['areas'][rand(0,4)]))."/novidades/post/index.php";
-                                    echo "?t=".str_replace(' ','+',$news['titles'][$i])."&img=carouselteste0".rand(1,3).".png&dt="
-                                    .$news['dates'][rand(0,4)]."+horas+atrás' class='col-4 d-block my-auto p-0 pr-md-3'>\n";
-                                    echo "<picture>\n<img class='rounded img-fluid' alt='".$news['titles'][$i]."' src='";
-                                    echo $corredor."img/carouselteste0".rand(1,3).".png'>\n</picture>\n</a>\n";
-                                    echo "<div class='col-8 h-auto d-flex flex-column pl-2 pr-0'><div class='my-auto'><a href='";
-                                    echo $corredor.strtolower(pato($news['areas'][rand(0,4)]))."/novidades/post/index.php";
-                                    echo "?t=".str_replace(' ','+',$news['titles'][$i])."&img=carouselteste0".rand(1,3).".png&dt="
-                                    .$news['dates'][rand(0,4)]."+horas+atrás' class='text-body'>\n";
-                                    echo "<h3 class='h5 d-inline font-weight-bold'>".$news['titles'][$i]."</h3></a></div></div></div></article>";
-                                }
-                            ?>
-                        </div>
-                    </div>
                 </aside>
             </div>
         </div>
